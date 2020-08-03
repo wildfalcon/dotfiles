9eb372b9de0af8f29595acf495e9663ffdb7de1f
Laurie Young <laurie@wildfalcon.com>
2020-08-03 12:49:40 +0100

    Add tmux.conf

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ ln -svf ".dotfiles/git-completion.bash" ~/.git-completion.bash
 ln -svf ".dotfiles/git-prompt.sh" ~/.git-prompt.sh
 ln -svf ".dotfiles/gitconfig" ~/.gitconfig
 ln -svf ".dotfiles/profile" ~/.profile
+ln -svf ".dotfiles/tmux.conf" ~/.tmux.conf
 ln -svf $HOME/.dotfiles/vscode.settings.json $HOME/Library/"Application Support"/Code/User/settings.json
 echo "...done!"
