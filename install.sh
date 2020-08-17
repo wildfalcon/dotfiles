ba92d394e716ebc715ef8e6e4e592083f0594d24
Laurie Young <laurie@wildfalcon.com>
2020-08-17 10:33:38 +0200

    Added vim config

--- install.sh
+++ install.sh
@@ -8,5 +8,6 @@ ln -svf ".dotfiles/git-prompt.sh" ~/.git-prompt.sh
 ln -svf ".dotfiles/gitconfig" ~/.gitconfig
 ln -svf ".dotfiles/profile" ~/.profile
 ln -svf ".dotfiles/tmux.conf" ~/.tmux.conf
+ln -svf ".dotfiles/vimrc" ~/.vimrc
 ln -svf $HOME/.dotfiles/vscode.settings.json $HOME/Library/"Application Support"/Code/User/settings.json
 echo "...done!"
